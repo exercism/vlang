dc87fa1213f554ff74f99cf80a4f2b8fafdc21a9
Ethan <1ethanhansen@protonmail.com>
2022-12-23 12:12:46 -0800

    feat: add robot name exercise

---
+++ exercises/practice/robot-name/robot-name.v
@@ -0,0 +1,21 @@
+module main
+
+// you'll probably want this
+import rand
+
+// define the Robot struct here
+struct Robot {
+
+}
+
+// we need a place to store all these robots!
+// make sure to update all the <TYPE>s to match
+// this should probably be an array or a map ;)
+fn create_robot_storage() <TYPE> {
+}
+
+fn create_robot(mut robots <TYPE>) Robot {
+}
+
+fn (mut r Robot) reset(mut robots <TYPE>) {
+}
