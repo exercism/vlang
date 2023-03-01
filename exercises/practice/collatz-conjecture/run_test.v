aa59643649f1782dee31e350e8ad9cb43276594c
André Santos <andrerfcsantos@gmail.com>
2023-03-01 22:39:29 +0000

    v fmt -w

--- exercises/practice/collatz-conjecture/run_test.v
+++ exercises/practice/collatz-conjecture/run_test.v
@@ -4,7 +4,7 @@ fn test_zero_steps_for_one() {
 	if res := collatz(1) {
 		assert res == 0
 	} else {
-		assert false, "collatz(1) should return 0, not an error"
+		assert false, 'collatz(1) should return 0, not an error'
 	}
 }
 
@@ -12,7 +12,7 @@ fn test_divide_if_even() {
 	if res := collatz(16) {
 		assert res == 4
 	} else {
-		assert false, "collatz(16) should return 4, not an error"
+		assert false, 'collatz(16) should return 4, not an error'
 	}
 }
 
@@ -20,7 +20,7 @@ fn test_even_and_odd_steps() {
 	if res := collatz(12) {
 		assert res == 9
 	} else {
-		assert false, "collatz(12) should return 9, not an error"
+		assert false, 'collatz(12) should return 9, not an error'
 	}
 }
 
@@ -28,13 +28,13 @@ fn test_large_number_of_even_and_odd_steps() {
 	if res := collatz(1000000) {
 		assert res == 152
 	} else {
-		assert false, "collatz(1000000) should return 152, not an error"
+		assert false, 'collatz(1000000) should return 152, not an error'
 	}
 }
 
 fn test_zero_is_an_error() {
 	if res := collatz(0) {
-		assert false, "collatz(0) should return an error, not $res"
+		assert false, 'collatz(0) should return an error, not ${res}'
 	} else {
 		assert true
 	}
@@ -42,7 +42,7 @@ fn test_zero_is_an_error() {
 
 fn test_negative_value_is_an_error() {
 	if res := collatz(-15) {
-		assert false, "collatz(-15) should return an error, not $res"
+		assert false, 'collatz(-15) should return an error, not ${res}'
 	} else {
 		assert true
 	}
