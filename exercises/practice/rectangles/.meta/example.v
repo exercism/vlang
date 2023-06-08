0f2781edaa5d4d3b3fd887780f9151d4d34dc23e
Eric Willigers <keiravillekode@gmail.com>
2023-06-08 22:23:15 +1000

    Refactor rectangles example solution

--- exercises/practice/rectangles/.meta/example.v
+++ exercises/practice/rectangles/.meta/example.v
@@ -1,33 +1,44 @@
 module main
 
 fn rectangles(strings []string) int {
+	is_corner := fn [strings] (row int, col int) bool {
+		return strings[row][col] == u8(`+`)
+	}
+
+	is_horizontal := fn [strings] (row int, col int) bool {
+		return strings[row][col] == u8(`-`) || strings[row][col] == u8(`+`)
+	}
+
+	is_vertical := fn [strings] (row int, col int) bool {
+		return strings[row][col] == u8(`|`) || strings[row][col] == u8(`+`)
+	}
+
 	mut result := 0
 	for top in 0 .. (strings.len) {
 		for left in 0 .. (strings[0].len) {
-			if strings[top][left] != u8(`+`) {
+			if !is_corner(top, left) {
 				continue
 			}
 
 			for bottom in (top + 1) .. (strings.len) {
-				if strings[bottom][left] == u8(`|`) {
-					continue
-				}
-				if strings[bottom][left] != u8(`+`) {
+				if !is_vertical(bottom, left) {
 					break
+				}
+				if !is_corner(bottom, left) {
+					continue
 				}
 
 				for right in (left + 1) .. (strings[0].len) {
-					if (strings[top][right] != u8(`-`) && strings[top][right] != u8(`+`))
-						|| (strings[bottom][right] != u8(`-`) && strings[bottom][right] != u8(`+`)) {
+					if !is_horizontal(top, right) || !is_horizontal(bottom, right) {
 						break
 					}
-					if strings[top][right] == u8(`-`) || strings[bottom][right] == u8(`-`) {
+					if !is_corner(top, right) || !is_corner(bottom, right) {
 						continue
 					}
 
 					mut valid := true
 					for row in (top + 1) .. bottom {
-						if strings[row][right] != u8(`|`) && strings[row][right] != u8(`+`) {
+						if !is_vertical(row, right) {
 							valid = false
 							break
 						}
