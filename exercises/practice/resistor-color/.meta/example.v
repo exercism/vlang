8f3561241d76aa0b3330fb6a381d6bd2c3f9a45f
Eric Willigers <keiravillekode@gmail.com>
2023-06-13 19:49:54 +1000

    feat: Add Resistor Color (closes #112)

---
+++ exercises/practice/resistor-color/.meta/example.v
@@ -0,0 +1,18 @@
+module main
+
+fn color_code(color string) int {
+	return colors.index(color)
+}
+
+const colors = [
+	'black',
+	'brown',
+	'red',
+	'orange',
+	'yellow',
+	'green',
+	'blue',
+	'violet',
+	'grey',
+	'white',
+]
