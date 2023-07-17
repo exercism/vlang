f8707e43126e572b272b861cc916963201e53eaf
Michael <m.charlton@mac.com>
2023-07-17 11:26:18 +0100

    feat(armstrong numbers): function now rejects -ve args
    Query concerning negative Armstrong numbers resolved on [forum](https://forum.exercism.org/t/can-a-valid-armstrong-numbers-be-negative/6514),
    in short they cannot.
    
    More obvious, and safer, method of counting the number of digits in an
    integer found using a function from the vlang stdlib.

--- exercises/practice/armstrong-numbers/.meta/example.v
+++ exercises/practice/armstrong-numbers/.meta/example.v
@@ -1,10 +1,11 @@
 module main
 
-import math { log10, powi }
+import math { count_digits, powi }
 
 pub fn is_armstrong_number(number i64) bool {
-	num_of_digits := i64(log10(f64(number))) + 1
+	assert number >= 0, 'Armstrong numbers cannot be negative'
 
+	num_of_digits := count_digits(number)
 	mut sum := i64(0)
 
 	for div := number; div > 0; div /= 10 {
