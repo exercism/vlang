9e1851e0695435e8e3bf664265bd011b12a0cee0
kivi <vikas@posteo.de>
2023-08-21 00:08:59 +0200

    updated the expect value, last two chunks 7 chars and trailing spaces

--- exercises/practice/crypto-square/run_test.v
+++ exercises/practice/crypto-square/run_test.v
@@ -44,6 +44,6 @@ fn test_8_character_plaintext_results_in_3_chunks_the_last_one_with_a_trailing_s
 
 fn test_54_character_plaintext_results_in_7_chunks_the_last_two_with_trailing_spaces() {
 	phrase := 'If man was meant to stay on the ground, god would have given us roots.'
-	expect := 'imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau '
+	expect := 'isotnugr fmshdlio metegdvo aaagohet nnyrdans wtoowvu atnuoes  '
 	assert ciphertext(phrase) == expect
 }
