7c0b57ad0a7393bba548b39a2bb70d4fff765876
kivi <vikas@posteo.de>
2023-08-22 12:32:55 +0200

    it´ś 8 chunks, so changing name of test function
    and also reverting to original expect value

--- exercises/practice/crypto-square/run_test.v
+++ exercises/practice/crypto-square/run_test.v
@@ -42,8 +42,8 @@ fn test_8_character_plaintext_results_in_3_chunks_the_last_one_with_a_trailing_s
 	assert ciphertext(phrase) == expect
 }
 
-fn test_54_character_plaintext_results_in_7_chunks_the_last_two_with_trailing_spaces() {
+fn test_54_character_plaintext_results_in_8_chunks_the_last_two_with_trailing_spaces() {
 	phrase := 'If man was meant to stay on the ground, god would have given us roots.'
-	expect := 'isotnugr fmshdlio metegdvo aaagohet nnyrdans wtoowvu atnuoes  '
+	expect := 'imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau '
 	assert ciphertext(phrase) == expect
 }
