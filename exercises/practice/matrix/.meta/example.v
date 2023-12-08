f663348cd5c73db58a151d987d155501fdb1fee7
Eric Willigers <keiravillekode@gmail.com>
2023-12-08 21:08:15 +1100

    use split_into_lines

--- exercises/practice/matrix/.meta/example.v
+++ exercises/practice/matrix/.meta/example.v
@@ -1,17 +1,15 @@
 module main
 
-import strconv
-
 fn row(str string, index int) []int {
-	line := str.split('\n')[index - 1]
+	line := str.split_into_lines()[index - 1]
 	return line.split(' ').map(fn (s string) int {
-		return strconv.atoi(s) or { 0 }
+		return s.int()
 	})
 }
 
 fn column(str string, index int) []int {
-	lines := str.split('\n')
+	lines := str.split_into_lines()
 	return lines.map(fn [index] (line string) int {
-		return strconv.atoi(line.split(' ')[index - 1]) or { 0 }
+		return line.split(' ')[index - 1].int()
 	})
 }
