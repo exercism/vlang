1aac5d5838cc529fd81b8ef36787a17c49b45a80
Kah Goh <villastar@yahoo.com.au>
2024-03-11 01:56:38 +0800

    Add Parallel Letter Frequency exercise (#200)
    * Add Parallel Letter Frequency exercise
    
    * Bump up parallel-letter-frequency difficulty to 5
    
    Co-authored-by: Ethan Hansen <1ethanhansen@protonmail.com>
    
    ---------
    
    Co-authored-by: Ethan Hansen <1ethanhansen@protonmail.com>

---
+++ exercises/practice/parallel-letter-frequency/.meta/example.v
@@ -0,0 +1,31 @@
+module main
+
+import encoding.utf8
+
+fn count_letters(word string) map[rune]int {
+	mut result := map[rune]int{}
+	lower := utf8.to_lower(word)
+	for r in 0 .. utf8.len(lower) {
+		c := utf8.get_uchar(lower, r)
+		if utf8.is_letter(rune(c)) {
+			result[c]++
+		}
+	}
+	return result
+}
+
+fn calculate_frequencies(texts []string) map[rune]int {
+	mut tasks := []thread map[rune]int{}
+	mut frequencies := map[rune]int{}
+	for word in texts {
+		tasks << spawn count_letters(word)
+	}
+
+	counts := tasks.wait()
+	for _, entry in counts {
+		for letter, c in entry {
+			frequencies[letter] = frequencies[letter] + c
+		}
+	}
+	return frequencies
+}
