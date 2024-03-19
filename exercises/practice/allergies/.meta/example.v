bde6dffc209b971a874e1d8bb3601110ddbeca46
Kah Goh <villastar@yahoo.com.au>
2024-03-19 22:53:47 +0800

    Move allergies enum into allergies.v
    This is to put the solution into one file.

--- exercises/practice/allergies/.meta/example.v
+++ exercises/practice/allergies/.meta/example.v
@@ -1,23 +1,21 @@
 module main
 
+enum Allergen as u8 {
+	eggs         = 1
+	peanuts      = 2
+	shellfish    = 4
+	strawberries = 8
+	tomatoes     = 16
+	chocolate    = 32
+	pollen       = 64
+	cats         = 128
+}
+
 fn allergic_to(allergen Allergen, score u8) bool {
-	return mask(allergen) & score > 0
+	return u8(allergen) & score > 0
 }
 
 fn list(score u8) []Allergen {
 	return [Allergen.eggs, Allergen.peanuts, Allergen.shellfish, Allergen.strawberries,
-		Allergen.tomatoes, Allergen.chocolate, Allergen.pollen, Allergen.cats].filter(score & mask(it) > 0)
-}
-
-fn mask(allergen Allergen) u8 {
-	return match allergen {
-		.eggs { 1 }
-		.peanuts { 2 }
-		.shellfish { 4 }
-		.strawberries { 8 }
-		.tomatoes { 16 }
-		.chocolate { 32 }
-		.pollen { 64 }
-		.cats { 128 }
-	}
+		Allergen.tomatoes, Allergen.chocolate, Allergen.pollen, Allergen.cats].filter(score & int(it) > 0)
 }
--- exercises/practice/allergies/allergen.v
+++ exercises/practice/allergies/allergen.v
@@ -1,12 +1,1 @@
 module main
-
-enum Allergen as u8 {
-	eggs
-	peanuts
-	shellfish
-	strawberries
-	tomatoes
-	chocolate
-	pollen
-	cats
-}
