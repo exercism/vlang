275c1f63f39cd80f5bc4eefac5d9d2c43b0a3802
Kah Goh <villastar@yahoo.com.au>
2024-06-11 22:29:09 +0800

    Add yacht exercise (#218)

---
+++ exercises/practice/yacht/yacht.v
@@ -0,0 +1,22 @@
+module main
+
+import arrays
+
+enum Category as u8 {
+	yacht
+	ones
+	twos
+	threes
+	fours
+	fives
+	sixes
+	full_house
+	four_of_a_kind
+	little_straight
+	big_straight
+	choice
+}
+
+fn score(category Category, rolls []u8) int {
+	// Please implement the score function
+}
