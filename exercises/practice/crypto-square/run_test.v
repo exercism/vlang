37412e5376e3d462ae38704f7b5b5425bb9f89b9
kivi <vikas@posteo.de>
2025-04-16 23:28:25 +0200

    fix:the expected value has 8 chunks, name test function should reflect that, changing from 7 to 8 (#150)

--- exercises/practice/crypto-square/run_test.v
+++ exercises/practice/crypto-square/run_test.v
@@ -42,7 +42,7 @@ fn test_8_character_plaintext_results_in_3_chunks_the_last_one_with_a_trailing_s
 	assert ciphertext(phrase) == expect
 }
 
-fn test_54_character_plaintext_results_in_7_chunks_the_last_two_with_trailing_spaces() {
+fn test_54_character_plaintext_results_in_8_chunks_the_last_two_with_trailing_spaces() {
 	phrase := 'If man was meant to stay on the ground, god would have given us roots.'
 	expect := 'imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau '
 	assert ciphertext(phrase) == expect
